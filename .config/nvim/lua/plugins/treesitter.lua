5be1e7ad95771aee9156ed0c932aded1a8aa9b4a
Abhigyan Madhukalya <amadhukalya2005@gmail.com>
2024-02-10 19:04:38 +0530

    feat: added neovim config

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "astro",
+        "cmake",
+        "cpp",
+        "css",
+        "gitignore",
+        "graphql",
+        "php",
+        "rust",
+        "sql",
+        "python",
+        "bash",
+        "toml",
+      },
+    },
+  },
+}
